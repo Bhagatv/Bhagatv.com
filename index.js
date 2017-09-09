5456368c2a228968df2f2e6b62bc3f9dd9653208
Bhagatv <bhagatv@uci.edu>
2017-09-09 15:20:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ for (i = 0; i < x.length; i++) {
 document.getElementsByClassName('active')[0].style.color = 'black';
 
   })
-  .on('leave', function() {
+  /*.on('leave', function() {
    document.getElementById('code').classList.remove("active");
     document.getElementById('home').classList.add("active");
 document.getElementsByClassName("active")[0].style.color='white';
@@ -46,7 +46,7 @@ for (i = 0; i < x.length; i++) {
 
 
 
-  })
+  })*/
   .setTween(tween)
   .addTo(scrollMagicController);
 
