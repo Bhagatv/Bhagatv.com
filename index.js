431a3d7f62f57bc5dd1585c8512006b4935eedf6
Bhagatv <bhagatv@uci.edu>
2017-09-09 15:47:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,33 @@ var tween = TweenMax.to('.button', 0.1, {
     offset: 0, /* offset the trigger 150px below #scene's top */
     
     
-  })$("#lol").typed({
+  }).on('start', function() {
+   document.getElementById('code').classList.add("active");
+    document.getElementById('home').classList.remove("active");
+    document.getElementsByClassName('active')[0].style.backgroundColor = 'white';
+    
+    var x = document.getElementsByClassName("button");
+var i;
+for (i = 0; i < x.length; i++) {
+    x[i].style.color = "white";
+}
+document.getElementsByClassName('active')[0].style.color = 'black';
+  })
+  .on('leave', function() {
+   document.getElementById('code').classList.remove("active");
+    document.getElementById('home').classList.add("active");
+document.getElementsByClassName("active")[0].style.color='white';
+  var x = document.getElementsByClassName("button");  
+    var i;
+for (i = 0; i < x.length; i++) {
+    x[i].style.color = "black";
+    x[i].style.backgroundColor='';
+}
+  })
+  .setTween(tween)
+  .addTo(scrollMagicController);
+scene.addIndicators();
+  $("#lol").typed({
 		strings: ["Hello...", "My name is Veeral Bhagat..", "I am a second year computer science major.."],
   typeSpeed:50,
   loop:true,
